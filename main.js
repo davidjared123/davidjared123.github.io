dc24f14f2c1f2d09f49766cf903bfc9f484be31e
David Perez <davidjared123@gmail.com>
2021-03-30 15:39:46 -0400

    error email added

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-$(document).ready(function(){
-    var $cabecera = $('#header');
-    var previousScroll = 0;
-    $(window).scroll(function(event){
-       var scroll = $(this).scrollTop();
-       if (scroll > previousScroll && scroll > 200){
-           $cabecera.addClass('bgcolor');
-       } else {
-           $cabecera.removeClass('bgcolor');
-       }
-       previousScroll = scroll;    });
- 
-  });
+const form = document.querySelector('.formulario');
+const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+       
+form.addEventListener('submit', e => {
+    //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+    e.preventDefault()
+    if (e.target.email.value.trim() === '' || emailRegex.test(e.target.value)) {
+        form.classList.add('form--error')
+        e.preventDefault()
+    }
+    
+    console.log(e.target.email.value)
+});
+
+form.email.addEventListener('focus', ()=>{
+    form.classList.remove('form--error')
+})
